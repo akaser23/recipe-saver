e5cfb55f2fd9609704989f73a008d22b0456ffd4
Anna Kaser <kaserann96@gmail.com>
2021-03-28 18:02:27 -0600

    npm installation

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./controllers');
+// const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const path = require('path');
